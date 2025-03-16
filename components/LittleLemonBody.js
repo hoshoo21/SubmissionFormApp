2eb904e3341e82a2eb5a2860a9157df5326d8afa
hoshoo21 <c_gawera@yahoo.com>
2025-03-16 05:09:54 -0700

    maintaining code

--- components/LittleLemonBody.js
+++ components/LittleLemonBody.js
@@ -3,21 +3,11 @@ import {Text, View, StyleSheet, ScrollView} from 'react-native';
 
 const LittleLemonBody =(props)=>{
         return (
-            <View style={{ flex:1, 
-                           paddingTop:40,
-                           paddingLeft:20,
-                          
-
-
-                }}>
+            <View style={styles.viewStyle}>
                 <Text style={styles.textStyle}>Registration Form </Text>
-               <ScrollView style={{
-                    flex:7.5,
-                    
-                }               
-               } 
-                indicatorStyle ="white"
-                indicatorStyle = 
+               <ScrollView style={styles.scrollViewStyle}
+             
+                indicatorStyle ={"white"}
                >
                     <Text style ={{...styles.textStyle,...{paddingTop:100}}}>
                         Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!
@@ -34,6 +24,17 @@ const styles = StyleSheet.create({
         fontSize : 58,
         color : "white",
         textAlign :'center',
+    },
+    scrollViewStyle :{
+            flex:1,
+            
+    },
+     viewStyle :{ flex:1, 
+        paddingTop:40,
+        paddingLeft:20,
+       
+
+
     }
 });
 
